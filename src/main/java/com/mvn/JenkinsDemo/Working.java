baf3e735c29757741d367bf7a260d663f7511642
Ranjitha Rajaraman <ranjithaclg@gmail.com>
2024-04-30 10:09:50 +0530

    First Commit

---
+++ src/main/java/com/mvn/JenkinsDemo/Working.java
@@ -0,0 +1,13 @@
+package com.mvn.JenkinsDemo;
+
+public class Working {
+
+	public String addToLog(String name) {
+		return "Adding Log for " + name;
+	}
+
+	public String addLogin(String loginName) {
+		return "Adding Login Log for " + loginName;
+	}
+
+}
